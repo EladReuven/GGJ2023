24d09ef74c729c5fac7a27dea94ef3e9150b67a0
Yossi <y.reuven9@gmail.com>
2023-02-03 11:01:15 +0200

    added menu on esc

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private Button ResumeButton;
+    [SerializeField] private Button ExitButton;
+    [SerializeField] private CanvasRenderer pauseMenu;
+
+    void TogglePause()
+    {
+        Time.timeScale=Time.timeScale==0?1:0;
+    }
+    void TogglePauseMenunOpen()
+    {
+        TogglePause();
+        TogglePauseMenuVisible();
+    }
+    void TogglePauseMenuVisible()
+    {
+        pauseMenu.gameObject.SetActive(!pauseMenu.gameObject.activeSelf);
+    }
+    void SwitchToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExitButton.onClick.AddListener(SwitchToMainMenu);
+        ResumeButton.onClick.AddListener(TogglePauseMenunOpen);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            TogglePauseMenuVisible();
+        }
+
+    }
+}
