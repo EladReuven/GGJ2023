348d9bd48a689ea0d3d1a7fe40ab4a0b94b9bf02
EladReuven <102731503+EladReuven@users.noreply.github.com>
2023-02-03 22:27:15 +0200

    moved roots to root file and added to game manager
    star power, add ammo, lose hp, and so on

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,10 +6,16 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public int score = 0;
+
     public int maxHP = 10;
     public int currentHP;
-    public int maxAmmo = 10;
+    public int maxAmmo = 5;
     public int currentAmmo;
+
+    public bool starPower = false;
+    public float starPowerDuration = 0;
+    public float starPowerBonus = 3f;
 
 
     void Awake()
@@ -17,11 +23,11 @@ public class GameManager : MonoBehaviour
         if(Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
+            return;
         }
     }
 
@@ -35,6 +41,39 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(currentHP < 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+
+        if(starPowerDuration > 0)
+        {
+            starPowerDuration -= Time.deltaTime;
+        }
+        else
+        {
+            starPower = false;
+        }
+
+    }
+
+
+    public void StarPower()
+    {
+        starPowerDuration += starPowerBonus;
+        starPower = true;
+    }
+
+    public void AddAmmo()
+    {
+        if(currentAmmo == maxAmmo)
+        {
+            Debug.Log("Max Ammo");
+        }
+        else
+        {
+            currentAmmo++;
+        }
     }
 }
