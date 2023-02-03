161d0edf42c3f39dac22136b9d4c3698da7026b8
Yossi <y.reuven9@gmail.com>
2023-02-04 00:18:12 +0200

    bottom side not working

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,27 +7,40 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] GameObject turret;
-    [SerializeField] GameObject fox;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] List<GameObject> airEnemies;
+    [SerializeField] List<GameObject> groundEnemies;
+    public float radius;
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    public void SpawnEnemey(GameObject unit,float distance)
-    {
-        SpawnEnemyInternal(unit, distance);
-    }
-    private void SpawnEnemyInternal(GameObject unit,float distance)
+    public void SpawnRandomEnemey()
     {
         float rand = UnityEngine.Random.value;
-        double radians = (rand*150/360 * 2 * Math.PI) +(285f/360f)*2*Math.PI;
-        Vector3 v = getv(radians)*distance;
+        double deg = (rand * 180) + 270f;
+        double radians = deg / 360f * 2 * Math.PI;
+        if ((deg >= 270 && deg < 285) || (deg <= 450 && deg > 435))
+        {
+            SpawnRandomEnemyGround(radians);
+        }else
+        {
+            SpawnRandomEnemyAir(radians);
+        }
+    }
+    private void SpawnRandomEnemyGround(double radians)
+    {
+        System.Random rand = new System.Random();
+        GameObject enemy = groundEnemies[rand.Next(1,groundEnemies.Count)-1];
+        SpawnEnemy(enemy,radians);
+    }
+    private void SpawnRandomEnemyAir(double radians)
+    {
+        System.Random rand = new System.Random();
+        GameObject enemy = airEnemies[rand.Next(1, airEnemies.Count) - 1];
+        SpawnEnemy(enemy, radians);
+    }
+
+
+    private void SpawnEnemy(GameObject unit,double radians) { 
+    
+        Vector3 v = getv(radians)*this.radius;
         Quaternion quat = Quaternion.FromToRotation(turret.transform.position,  v); //the direction from spawn to turret
         GameObject obj = Instantiate(unit,v,quat);
     }
